cdc843a6d1d575191565398dbc8a356c8d26ddbc
Juliano Seus <JulianoSeusDev@outlook.com>
2021-02-12 10:26:01 -0300

    enviando dados para a API

--- users/src/views/Register.vue
+++ users/src/views/Register.vue
@@ -7,23 +7,46 @@
         <div class="columns is-centered">
             <div class="column is-half">
                 <p>Nome</p>
-                <input class="input" type="text" placeholder="Nome do usuário">
+                <input v-model="name" class="input" type="text" placeholder="Nome do usuário">
 
                 <p>E-mail</p>
-                <input class="input" type="email" placeholder="email@email.com">
+                <input v-model="email" class="input" type="email" placeholder="email@email.com">
 
                 <p>Senha</p>
-                <input class="input" type="password" placeholder="********">
+                <input v-model="password" class="input" type="password" placeholder="********">
 
                 <hr>
-                <button class="button is-success">Cadastrar</button>
+                <button @click="register" class="button is-success">Cadastrar</button>
             </div>
         </div>  
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+    data(){
+        return {
+            name: '',
+            password: '',
+            email: ''
+        }
+    },
+    methods: {
+        register(){
+            axios.post("http://localhost:8686/user",{
+                name: this.name,
+                password: this.password,
+                email: this.email
+            }).then(res => {
+                console.log(res);
+            }).catch(err => {
+                var msgErro = err.response.data.err
+                console.log(msgErro);
+            });
+
+        }
+    }
 }
 </script>
 
