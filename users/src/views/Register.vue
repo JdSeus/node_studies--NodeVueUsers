0754df59972d0da9497121cba544a68c8b78cc9e
Juliano Seus <JulianoSeusDev@outlook.com>
2021-02-12 10:01:44 -0300

    formulario de cadastro

---
+++ users/src/views/Register.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <br>
+        <br>
+        <h2>Registro de usuário!</h2>
+        <hr>
+        <div class="columns is-centered">
+            <div class="column is-half">
+                <p>Nome</p>
+                <input class="input" type="text" placeholder="Nome do usuário">
+
+                <p>E-mail</p>
+                <input class="input" type="email" placeholder="email@email.com">
+
+                <p>Senha</p>
+                <input class="input" type="password" placeholder="********">
+
+                <hr>
+                <button class="button is-success">Cadastrar</button>
+            </div>
+        </div>  
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
