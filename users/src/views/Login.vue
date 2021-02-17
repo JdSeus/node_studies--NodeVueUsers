c7feb559d569ef84bf7006cc462dd423dc8c88a0
Juliano Seus <JulianoSeusDev@outlook.com>
2021-02-17 08:45:46 -0300

    front do Login

--- users/src/views/Login.vue
+++ users/src/views/Login.vue
@@ -2,7 +2,7 @@
     <div>
         <br>
         <br>
-        <h2>Registro de usuário!</h2>
+        <h2>Login</h2>
         <hr>
         <div class="columns is-centered">
             <div class="column is-half">
@@ -13,8 +13,6 @@
                     </div>
                     <hr>
                 </div>
-                <p>Nome</p>
-                <input v-model="name" class="input" type="text" placeholder="Nome do usuário">
 
                 <p>E-mail</p>
                 <input v-model="email" class="input" type="email" placeholder="email@email.com">
@@ -23,38 +21,25 @@
                 <input v-model="password" class="input" type="password" placeholder="********">
 
                 <hr>
-                <button @click="register" class="button is-success">Cadastrar</button>
+                <button @click="login" class="button is-success">Logar</button>
             </div>
         </div>  
     </div>
 </template>
 
 <script>
-import axios from 'axios'
+//import axios from 'axios'
 export default {
     data(){
         return {
-            name: '',
             password: '',
             email: '',
             error: undefined,
         }
     },
     methods: {
-        register(){
-            axios.post("http://localhost:8686/user",{
-                name: this.name,
-                password: this.password,
-                email: this.email
-            }).then((res) => {
-                console.log(res);
-                this.$router.push({name: 'Home'});
-            }).catch((err) => {
-                if (err.response.data.err != undefined) {
-                    this.error = err.response.data.err;
-                }
-            });
-
+        login(){
+            console.log("login!")
         }
     }
 }
