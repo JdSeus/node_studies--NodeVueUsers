f0dac007021416117e7261986b43285beefe0ca2
Juliano Seus <JulianoSeusDev@outlook.com>
2021-02-17 09:32:57 -0300

    autorizacao no back end

--- users/src/router/index.js
+++ users/src/router/index.js
@@ -4,6 +4,32 @@ import Register from '../views/Register.vue'
 import Login from '../views/Login.vue'
 import Users from '../views/Users.vue'
 
+import axios from 'axios'
+
+function AdminAuth(to, from, next) {
+  if(localStorage.getItem('token') != undefined) {
+        
+    var req = {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem('token')
+      }
+    }
+
+    console.log(req);
+
+    axios.post("http://localhost:8686/validate",{},req).then(res => {
+      console.log(res);
+      next()
+    }).catch(err => {
+      console.log(err.response);
+      next("/login");
+    });
+
+  } else {
+    next("/login");
+  }
+}
+
 const routes = [
   {
     path: '/',
@@ -24,13 +50,7 @@ const routes = [
     path:'/admin/users',
     name: 'Users',
     component: Users,
-    beforeEnter: (to, from, next) => {
-      if(localStorage.getItem('token') != undefined) {
-        next();
-      } else {
-        next("/login");
-      }
-    }
+    beforeEnter: AdminAuth
   },
   {
     path: '/about',
